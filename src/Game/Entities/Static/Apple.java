b8d3c9c0e6158843273a3dc2119d34bc54b419f4
Jose A. Lopez Martínez <jose.lopez100@upr.edu>
2019-09-12 20:50:40 -0400

    Added isGood() method that checks if apples are rotten or not.

--- src/Game/Entities/Static/Apple.java
+++ src/Game/Entities/Static/Apple.java
@@ -1,5 +1,6 @@
 package Game.Entities.Static;
 
+import Game.Entities.Dynamic.Player;
 import Main.Handler;
 
 /**
@@ -11,12 +12,22 @@ public class Apple {
 
     public int xCoord;
     public int yCoord;
+    
 
     public Apple(Handler handler,int x, int y){
         this.handler=handler;
         this.xCoord=x;
         this.yCoord=y;
     }
-
+    
+//Checks if apple is rotten
+    public static boolean isGood() {
+    	if(Player.stepCounter > 180) {
+    		return false;
+    	}
+    	else
+    		return true;
+    }
+   
 
 }
